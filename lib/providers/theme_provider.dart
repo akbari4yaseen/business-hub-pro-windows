8949b40e6b6f0d747701b7bf39a64c26bddb3444
akbari01 <akbari01.dev@gmail.com>
2025-03-14 05:11:11 +0430

    app_theme optimized

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import '../themes/app_theme.dart';
 
 class ThemeProvider with ChangeNotifier {
-  bool _isDarkMode = false;
+  bool _isDarkMode = false; // Stores the current theme mode
 
+  // Getter to check if dark mode is enabled
   bool get isDarkMode => _isDarkMode;
 
-  ThemeData get currentTheme => _isDarkMode ? _darkTheme : _lightTheme;
+  // Returns the appropriate theme based on the current mode
+  ThemeData get currentTheme =>
+      _isDarkMode ? AppTheme.darkTheme : AppTheme.lightTheme;
 
-// Bottom navigation bar colors
+  // Bottom navigation bar colors
   Color get bottomNavBackgroundColor =>
       _isDarkMode ? Colors.grey[900]! : Colors.white;
   Color get bottomNavSelectedItemColor =>
@@ -16,90 +20,30 @@ class ThemeProvider with ChangeNotifier {
   Color get bottomNavUnselectedItemColor =>
       _isDarkMode ? Colors.grey : Colors.black87.withOpacity(0.7);
 
+  // AppBar Colors
   Color get appBarTextColor => _isDarkMode ? Colors.white : Colors.grey[900]!;
   Color get appBarBackgroundColor =>
       _isDarkMode ? Colors.grey[900]! : Colors.white;
 
-  // System navigation bar color
+  // System navigation bar color (used for Android system UI customization)
   Color get systemNavBarColor => _isDarkMode ? Colors.grey[900]! : Colors.white;
 
+  // Toggles between dark and light mode
   void toggleTheme() {
     _isDarkMode = !_isDarkMode;
-    updateSystemNavigationBarColor(); // Update system navigation bar color
-    notifyListeners();
+    updateSystemNavigationBarColor(); // Update system UI colors accordingly
+    notifyListeners(); // Notify all listeners about the theme change
   }
 
+  // Updates the system navigation bar color to match the theme
   void updateSystemNavigationBarColor() {
     SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle(
-        systemNavigationBarColor:
-            systemNavBarColor, // Set system navigation bar color
+        systemNavigationBarColor: systemNavBarColor, // Change the nav bar color
         systemNavigationBarIconBrightness: _isDarkMode
             ? Brightness.light
-            : Brightness.dark, // Set icon brightness
+            : Brightness.dark, // Adjust icon brightness
       ),
     );
   }
-
-  static final ThemeData _lightTheme = ThemeData(
-      primarySwatch: Colors.blue,
-      primaryColor: Colors.blue,
-      cardTheme: CardTheme(color: Colors.white),
-      brightness: Brightness.light,
-      fontFamily: "Vazir",
-      scaffoldBackgroundColor: Colors.white70,
-      bottomSheetTheme: BottomSheetThemeData(
-        backgroundColor: Colors.white,
-      ),
-      floatingActionButtonTheme: FloatingActionButtonThemeData(
-          backgroundColor: Colors.blue,
-          foregroundColor: Colors.white,
-          splashColor: Colors.blue[900],
-          hoverColor: Colors.blue[300]),
-      tabBarTheme: TabBarTheme(
-        labelColor: Colors.blue,
-        indicatorColor: Colors.blue,
-      ),
-      appBarTheme: AppBarTheme(
-        color: Colors.white,
-        iconTheme: IconThemeData(color: Colors.grey[900]),
-        titleTextStyle: TextStyle(
-            color: Colors.grey[900], fontFamily: "IRANSans", fontSize: 14),
-      ),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ButtonStyle(
-              elevation: WidgetStatePropertyAll(0),
-              backgroundColor: WidgetStatePropertyAll(Colors.blue),
-              foregroundColor: WidgetStatePropertyAll(Colors.white),
-              shape: WidgetStatePropertyAll(RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(6)))))));
-
-  static final ThemeData _darkTheme = ThemeData(
-      primarySwatch: Colors.blue,
-      primaryColor: Colors.blue,
-      brightness: Brightness.dark,
-      fontFamily: "Vazir",
-      tabBarTheme: TabBarTheme(
-        labelColor: Colors.blue,
-        indicatorColor: Colors.blue,
-      ),
-      floatingActionButtonTheme: FloatingActionButtonThemeData(
-          backgroundColor: Colors.blue,
-          foregroundColor: Colors.white,
-          splashColor: Colors.blue[900],
-          hoverColor: Colors.blue[300]),
-      scaffoldBackgroundColor: Colors.grey[900],
-      appBarTheme: AppBarTheme(
-        color: Colors.grey[900],
-        iconTheme: IconThemeData(color: Colors.white),
-        titleTextStyle: TextStyle(
-            color: Colors.white, fontFamily: "IRANSans", fontSize: 14),
-      ),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ButtonStyle(
-              elevation: WidgetStatePropertyAll(0),
-              backgroundColor: WidgetStatePropertyAll(Colors.blue),
-              foregroundColor: WidgetStatePropertyAll(Colors.white),
-              shape: WidgetStatePropertyAll(RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(6)))))));
 }
--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -1,1 +1,75 @@
+import 'package:flutter/material.dart';
 
+class AppTheme {
+  static final ThemeData lightTheme = ThemeData(
+    primarySwatch: Colors.blue,
+    primaryColor: Colors.blue,
+    cardTheme: CardTheme(color: Colors.white),
+    brightness: Brightness.light,
+    fontFamily: "Vazir",
+    scaffoldBackgroundColor: Colors.white70,
+    bottomSheetTheme: BottomSheetThemeData(backgroundColor: Colors.white),
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      backgroundColor: Colors.blue,
+      foregroundColor: Colors.white,
+      splashColor: Colors.blue[900],
+      hoverColor: Colors.blue[300],
+    ),
+    tabBarTheme: TabBarTheme(
+      labelColor: Colors.blue,
+      indicatorColor: Colors.blue,
+    ),
+    appBarTheme: AppBarTheme(
+      color: Colors.white,
+      iconTheme: IconThemeData(color: Colors.grey[900]),
+      titleTextStyle: TextStyle(
+          color: Colors.grey[900], fontFamily: "IRANSans", fontSize: 14),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        elevation: WidgetStatePropertyAll(0),
+        backgroundColor: WidgetStatePropertyAll(Colors.blue),
+        foregroundColor: WidgetStatePropertyAll(Colors.white),
+        shape: WidgetStatePropertyAll(
+          RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(6))),
+        ),
+      ),
+    ),
+  );
+
+  static final ThemeData darkTheme = ThemeData(
+    primarySwatch: Colors.blue,
+    primaryColor: Colors.blue,
+    brightness: Brightness.dark,
+    fontFamily: "Vazir",
+    tabBarTheme: TabBarTheme(
+      labelColor: Colors.blue,
+      indicatorColor: Colors.blue,
+    ),
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      backgroundColor: Colors.blue,
+      foregroundColor: Colors.white,
+      splashColor: Colors.blue[900],
+      hoverColor: Colors.blue[300],
+    ),
+    scaffoldBackgroundColor: Colors.grey[900],
+    appBarTheme: AppBarTheme(
+      color: Colors.grey[900],
+      iconTheme: IconThemeData(color: Colors.white),
+      titleTextStyle:
+          TextStyle(color: Colors.white, fontFamily: "IRANSans", fontSize: 14),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        elevation: WidgetStatePropertyAll(0),
+        backgroundColor: WidgetStatePropertyAll(Colors.blue),
+        foregroundColor: WidgetStatePropertyAll(Colors.white),
+        shape: WidgetStatePropertyAll(
+          RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(6))),
+        ),
+      ),
+    ),
+  );
+}
