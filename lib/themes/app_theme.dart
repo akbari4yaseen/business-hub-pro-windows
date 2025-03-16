05a2f5fa2c8c55603aa48fd451f82e0c1b870b34
akbari01 <akbari01.dev@gmail.com>
2025-03-16 12:33:33 +0430

    app color optimized

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -2,6 +2,18 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static final ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme(
+      brightness: Brightness.light,
+      primary: Colors.blue,
+      onPrimary: Colors.white, // Ensures text on blue is readable
+      secondary: const Color.fromARGB(255, 187, 222, 251),
+      onSecondary: Colors.black87, // Ensures contrast on secondary color
+      error: const Color.fromARGB(255, 239, 83, 80),
+      onError: const Color.fromARGB(255, 183, 28, 28),
+      surface: const Color.fromARGB(255, 255, 255, 255),
+      onSurface: Colors.grey.shade800,
+    ),
     primarySwatch: Colors.blue,
     primaryColor: Colors.blue,
     cardTheme: CardTheme(color: Colors.white),
@@ -24,7 +36,10 @@ class AppTheme {
       color: Colors.white,
       iconTheme: IconThemeData(color: Colors.grey[900]),
       titleTextStyle: TextStyle(
-          color: Colors.grey[900], fontFamily: "IRANSans", fontSize: 14),
+        color: Colors.grey[900],
+        fontFamily: "IRANSans",
+        fontSize: 14,
+      ),
     ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ButtonStyle(
@@ -33,20 +48,49 @@ class AppTheme {
         foregroundColor: WidgetStatePropertyAll(Colors.white),
         shape: WidgetStatePropertyAll(
           RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(6))),
+            borderRadius: BorderRadius.all(Radius.circular(6)),
+          ),
         ),
       ),
     ),
   );
 
   static final ThemeData darkTheme = ThemeData(
-    primarySwatch: Colors.blue,
-    primaryColor: Colors.blue,
+    useMaterial3: true,
     brightness: Brightness.dark,
     fontFamily: "Vazir",
+    primarySwatch: Colors.blue,
+    primaryColor: Colors.blue,
+    colorScheme: ColorScheme(
+      brightness: Brightness.dark,
+      primary: Colors.blue,
+      onPrimary: Colors.white,
+      secondary: const Color.fromRGBO(
+          144, 202, 249, 1), // A lighter blue accent for dark mode
+      onSecondary: Colors.white,
+      error: const Color.fromRGBO(239, 83, 80, 1),
+      onError: Colors.black,
+      surface: Colors.grey.shade800,
+      onSurface: Colors.white70,
+    ),
+    cardTheme: CardTheme(color: Colors.grey.shade800),
+    drawerTheme:
+        DrawerThemeData(backgroundColor: Color.fromRGBO(48, 48, 48, 1)),
+    scaffoldBackgroundColor: Colors.grey.shade900,
+    bottomSheetTheme:
+        BottomSheetThemeData(backgroundColor: Color.fromRGBO(48, 48, 48, 1)),
     tabBarTheme: TabBarTheme(
       labelColor: Colors.blue,
       indicatorColor: Colors.blue,
+    ),
+    appBarTheme: AppBarTheme(
+      color: Colors.grey.shade900,
+      iconTheme: IconThemeData(color: Colors.white),
+      titleTextStyle: TextStyle(
+        color: Colors.white,
+        fontFamily: "IRANSans",
+        fontSize: 14,
+      ),
     ),
     floatingActionButtonTheme: FloatingActionButtonThemeData(
       backgroundColor: Colors.blue,
@@ -54,13 +98,6 @@ class AppTheme {
       splashColor: Colors.blue[900],
       hoverColor: Colors.blue[300],
     ),
-    scaffoldBackgroundColor: Colors.grey[900],
-    appBarTheme: AppBarTheme(
-      color: Colors.grey[900],
-      iconTheme: IconThemeData(color: Colors.white),
-      titleTextStyle:
-          TextStyle(color: Colors.white, fontFamily: "IRANSans", fontSize: 14),
-    ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ButtonStyle(
         elevation: WidgetStatePropertyAll(0),
@@ -68,7 +105,8 @@ class AppTheme {
         foregroundColor: WidgetStatePropertyAll(Colors.white),
         shape: WidgetStatePropertyAll(
           RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(6))),
+            borderRadius: BorderRadius.all(Radius.circular(6)),
+          ),
         ),
       ),
     ),
