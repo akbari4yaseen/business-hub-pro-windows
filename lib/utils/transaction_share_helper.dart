20bfd5fd6e8db9101b5cfc8da228ed1c35c25313
akbari01 <akbari01.dev@gmail.com>
2026-04-15 12:03:54 +0430

    removed arb duplictes

--- lib/utils/transaction_share_helper.dart
+++ lib/utils/transaction_share_helper.dart
@@ -5,7 +5,7 @@ import 'package:provider/provider.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:intl/intl.dart';
-import '../utils/date_formatters.dart';
+import 'date_formatters.dart';
 import '../providers/info_provider.dart';
 
 class TransactionShareHelper {
@@ -22,24 +22,28 @@ class TransactionShareHelper {
 
     // Extract and format transaction data
     final rawDate = transaction['date'];
-    final DateTime transactionDate = rawDate == null 
-        ? DateTime.now() 
-        : rawDate is String 
-            ? DateTime.parse(rawDate) 
+    final DateTime transactionDate = rawDate == null
+        ? DateTime.now()
+        : rawDate is String
+            ? DateTime.parse(rawDate)
             : rawDate as DateTime;
-    final date = formatLocalizedDateEnglishNumbers(context, transactionDate.toString());
+    final date =
+        formatLocalizedDateEnglishNumbers(context, transactionDate.toString());
     final amount = transaction['amount'] as num? ?? 0;
     final formattedAmount = NumberFormat('#,###.##').format(amount);
     final currency = transaction['currency'] as String? ?? '';
     final description = transaction['description'] as String? ?? '';
-    final type = (transaction['transaction_type'] as String?)?.toLowerCase() ?? '';
-    final transactionType = type == 'credit' ? loc.creditTransactionType : loc.debitTransactionType;
+    final type =
+        (transaction['transaction_type'] as String?)?.toLowerCase() ?? '';
+    final transactionType =
+        type == 'credit' ? loc.creditTransactionType : loc.debitTransactionType;
 
     // Build transaction details
     final buffer = StringBuffer();
     buffer.writeln(loc.shareTransactionGreeting(accountName ?? ''));
-    buffer.writeln(loc.shareTransactionMessage(formattedAmount, currency, transactionType, date));
-    
+    buffer.writeln(loc.shareTransactionMessage(
+        formattedAmount, currency, transactionType, date));
+
     if (description.isNotEmpty) {
       buffer.writeln('\n${loc.shareTransactionDescription(description)}');
     }
@@ -47,7 +51,7 @@ class TransactionShareHelper {
     // Add account balances if available
     if (accountBalances != null && accountBalances.isNotEmpty) {
       buffer.writeln('\n${loc.shareTransactionBalanceHeader}');
-      
+
       // Format each balance line
       accountBalances.forEach((currency, data) {
         final balance = data['summary']?['balance'] as num? ?? 0.0;
@@ -73,7 +77,7 @@ class TransactionShareHelper {
     Map<String, dynamic>? accountBalances,
   }) async {
     final message = await buildTransactionMessage(
-      context, 
+      context,
       transaction,
       accountName: accountName,
       accountBalances: accountBalances,
@@ -88,7 +92,7 @@ class TransactionShareHelper {
     Map<String, dynamic>? accountBalances,
   }) async {
     final message = await buildTransactionMessage(
-      context, 
+      context,
       transaction,
       accountName: accountName,
       accountBalances: accountBalances,
@@ -111,7 +115,7 @@ class TransactionShareHelper {
     Map<String, dynamic>? accountBalances,
   }) async {
     final message = await buildTransactionMessage(
-      context, 
+      context,
       transaction,
       accountName: accountName,
       accountBalances: accountBalances,
@@ -131,8 +135,8 @@ class TransactionShareHelper {
 
     // Fall back to regular share if WhatsApp is not available or no phone number
     await shareTransaction(
-      context, 
-      transaction, 
+      context,
+      transaction,
       accountName: accountName,
       accountBalances: accountBalances,
     );
